b9ee190789b6824015103d8b33265f28050466b6
BwuNi <bwuni@outlook.com>
2019-10-25 20:15:17 +0800

    sh : add vscode

--- sh/pi.init.sh
+++ sh/pi.init.sh
@@ -15,5 +15,15 @@ sudo apt-get install fcitx fcitx-googlepinyin fcitx-module-cloudpinyin fcitx-sun
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 
+# 树莓派安装VSCode
+# 按照网上说的装了很多次都不成功，其实官网里面有写：https://www.raspberrypi.org/forums/viewtopic.php?p=1440854
+
+
+sudo wget -qO - https://packagecloud.io/headmelted/codebuilds/gpgkey | sudo apt-key add -;
+
+wget --content-disposition https://packagecloud.io/headmelted/codebuilds/packages/debian/stretch/code-oss_1.29.0-1539702238_armhf.deb/download.deb
+
+sudo apt install ./code-oss_1.29.0-1539702238_armhf.deb
+
 # 重启树莓派即可
 reboot
